26725cef2fd82f24f9c853df92e0fcbae6837438
moon-to-mars <a01034972144@gmail.com>
2026-06-28 06:34:49 +0900

    chore: 프로젝트 셋업 (Vite + React + TypeScript + Tailwind)

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-gray-800">SNUTI 프로토타입</h1>
+    </div>
+  )
+}
+
+export default App
